26f24ad90d4b5d68d1759bc154d9452366245470
kazamaru7049 <kyantyome.gassyuberu@gmail.com>
2026-04-02 08:51:19 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const axios = require('axios');
+
+const client = new Client({
+intents: [
+GatewayIntentBits.Guilds,
+GatewayIntentBits.GuildVoiceStates,
+GatewayIntentBits.GuildMembers,
+GatewayIntentBits.GuildMessages,
+GatewayIntentBits.MessageContent
+]
+});
+
+const TOKEN = "MTQ4ODg5OTY3ODgyNDAzODYyMA.Ge5ied.efy_4_0yPe6je-0LdM5R8S5iJmZSmPkcGumGZY";
+const GAS_URL = "https://script.google.com/macros/s/AKfycby7gWGxypngMUUGyTAjlbVnsSK2L4DCm87sP_MntNgD83oyGcZdozF0G4JGHURsCitA/exec";
+
+// 通話参加検知
+client.on('voiceStateUpdate', async (oldState, newState) => {
+if (!oldState.channel && newState.channel) {
+const name = newState.member.displayName;
+
+console.log(`${name} が通話参加`);
+
+await sendToSheet(name);
+}
+});
+
+// 発言検知
+client.on('messageCreate', async (message) => {
+if (message.author.bot) return;
+
+const name = message.member.displayName;
+
+console.log(`${name} が発言`);
+
+await sendToSheet(name);
+});
+
+// GAS送信処理
+async function sendToSheet(name) {
+try {
+await axios.post(GAS_URL, {
+name: name
+});
+} catch (e) {
+console.error("送信エラー", e);
+}
+}
+
+client.login(TOKEN);
